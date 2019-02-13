c1cc3fb3d8beaeca9f76d185f0fa5562365f1b79
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2019-02-13 17:48:40 -0500

    Update Interpreter.java

--- BrainF*ck Interpreter/Interpreter.java
+++ BrainF*ck Interpreter/Interpreter.java
@@ -8,15 +8,15 @@ public class Interpreter {
         }
         
         public void interpret(Parser parser) {
-            parser.parse();
             for(Command c : parser.cmd) {
                 if(c.label == '>') this.index++;
                 if(c.label == '<') this.index--;
                 if(c.label == '+') this.memory[this.index]++;
                 if(c.label == '-') this.memory[this.index]--;
-                if(c.label == '.') System.out.print((char) (this.memory[this.index]));
+                if(c.label == '.') System.out.print((char) this.memory[this.index]);
                 if(c.label == 'R') {
                     Parser subParser = new Parser(c.subData);
+                    subParser.parse();
                     while(this.memory[this.index] > 0) {
                         interpret(subParser);
                     }
