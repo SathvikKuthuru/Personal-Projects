f1ad6ed44e743c0d86eb90cd490a8e4809ee1cc0
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2019-10-19 17:18:24 -0400

    Update Test.java

--- Calculus/Test.java
+++ Calculus/Test.java
@@ -1,11 +1,13 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		Expression test = new Expression("Derive(x^2 + x/4 * x, x)");
-		System.out.println(test.getExpressionType());
-		System.out.println(test.getArgument());
-		System.out.println(test.getVariable());
-	}
+    public static void main(String[] args) {
+        Expression test = new Expression("((x+4)++(x+5), x)", CalculationType.DERIVATIVE);
+        System.out.println(test.getExpressionType());
+        System.out.println(test.getArgument());
+        System.out.println(test.getVariable());
+        SyntaxChecker syntaxChecker = new SyntaxChecker(test);
+        System.out.println(syntaxChecker.checkTotalValidity().getMessage());
+    }
 
 }
